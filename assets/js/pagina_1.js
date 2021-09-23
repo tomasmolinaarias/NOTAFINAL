14e9723eec5a62f258c76a7765d21e30b3ec26cb
tomasmolinaarias <tomasmolinaarias12@gmail.com>
2021-09-22 23:28:50 -0300

    casiperfecto

--- assets/js/pagina_1.js
+++ assets/js/pagina_1.js
@@ -3,7 +3,7 @@ var nombre = prompt ("Coloca tu NOMBRE!")
 var carrera = prompt ("¿En que carrera te encuentras?")
 
 //nota de aprobacion
-var nota_APR = prompt("¿cual es la nota de aprobacion", "7")
+var nota_APR = prompt("¿cual es la nota de aprobacion", "4.0")
 
 //pedir los modulos
 var modulo1 = prompt ("escribe el primer modulo")
@@ -11,19 +11,19 @@ var modulo2 = prompt ("escribe el segundo modulo")
 var modulo3 = prompt("escribe el tercer modulo")
 
 //pedir la nota del primer modulo
-var nM1_1 = prompt ("Coloca la primera nota del MODULO 1","1")
-var nM1_2 = prompt ("Coloca la segunda nota del MODULO 1","1")
-var nM1_3 = prompt ("Coloca la tercera nota del MODULO 1","1")
+var nM1_1 = prompt ("Coloca la primera nota del MODULO 1","1.0")
+var nM1_2 = prompt ("Coloca la segunda nota del MODULO 1","1.0")
+var nM1_3 = prompt ("Coloca la tercera nota del MODULO 1","1.0")
 
 //pedir la nota del segundo modulo
-var nM2_1 = prompt ("coloca la primera nota del MODULO 2","1")
-var nM2_2 = prompt ("coloca la segunda nota del MODULO 2","1")
-var nM2_3 = prompt ("coloca la tercera nota del MODULO 2","1")
+var nM2_1 = prompt ("coloca la primera nota del MODULO 2","1.0")
+var nM2_2 = prompt ("coloca la segunda nota del MODULO 2","1.0")
+var nM2_3 = prompt ("coloca la tercera nota del MODULO 2","1.0")
 
 //pedir la nota del tercer modulo
-var nM3_1 = prompt ("coloca la primera nota del MODULO 3","1")
-var nM3_2 = prompt ("coloca la segunda nota del MODULO 3","1")
-var nM3_3 = prompt ("coloca la tercera nota del MODULO 3","1")
+var nM3_1 = prompt ("coloca la primera nota del MODULO 3","1.0")
+var nM3_2 = prompt ("coloca la segunda nota del MODULO 3","1.0")
+//var nM3_3 = prompt ("coloca la tercera nota del MODULO 3","1.0")
 
 //pegamos  los h5 y le agregamos el nombre y la carrera
 document.write(`<div class="container"> <h5>Nombre:   ${nombre} </h5> </div>`)
@@ -39,9 +39,9 @@ nM2_1 = parseInt(nM2_1)
 nM2_2 = parseInt(nM2_2)
 nM2_3 = parseInt(nM2_3)
 //modulo 3
-nM3_1 = parseInt(nM3_1)
+nM3_1 = parseInt(nM3_1) 
 nM3_2 = parseInt(nM3_2)
-nM3_3 = parseInt(nM3_3)
+//nM3_3 = parseInt(nM3_3)
 
 //promedio
 //modulo1
@@ -49,16 +49,18 @@ var suma_m1 = nM1_1 + nM1_2 + nM1_3
 var divicion_m1 = suma_m1 / 3 
 var promedio_modulo1 = divicion_m1
 //modulo2
-var suma_m2 = nM2_1 + nM2_2 + nM3_3
+var suma_m2 = nM2_1 + nM2_2 + nM2_3
 var divicion_m2 = suma_m2 / 3
 var promedio_modulo2 = divicion_m2
 //modulo3
-var suma_m3 = nM3_1 + nM3_2 + nM3_3
+var suma_m3 = nM3_1 + nM3_2 
 var divicion_m3 = suma_m2 / 3
 var promedio_modulo3 = divicion_m3
 
 //restante
-var restante = suma_m3 % nota_APR
+var resta = nota_APR - nM3_1 - nM3_2  
+var restante = resta
+
 
 
 //tabla
@@ -94,7 +96,7 @@ document.write(`
                     <th scope="row">${modulo3}</th>
                     <td>${nM3_1}</td>
                     <td>${nM3_2}</td>
-                    <td>${nM3_3}</td>
+                    <td>-</td>
                     <td>${promedio_modulo3.toFixed(2)}</td>
                   </tr>
                 </tbody>
@@ -102,7 +104,7 @@ document.write(`
 `)
 document.write(`
 <div class="container">
-<p> para aprobar el ramo  ${modulo3} con nota ${nota_APR} necesitas obtener un ${restante.toFixed(0)} en la ultima nota </p>
+<p> para aprobar el ramo  ${modulo3} con nota ${nota_APR} necesitas obtener un ${restante.toFixed(2)} en la 3 nota </p>
 </div>
 
 `) 
